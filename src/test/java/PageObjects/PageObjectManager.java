7cba54f9546ea527703c9a81b4dcd7a802364d58
Viswashorish <viswanadh.ms@yahoo.com>
2025-09-28 10:41:06 +0530

    Save untracked files before switching

---
+++ src/test/java/PageObjects/PageObjectManager.java
@@ -0,0 +1,38 @@
+package PageObjects;
+
+import org.openqa.selenium.WebDriver;
+
+public class PageObjectManager 
+{
+	
+	WebDriver driver;
+	
+	LandingPage_PageObject landing;
+	
+	OffersPage_PageObject offers;
+	
+	public PageObjectManager(WebDriver driver)
+	{
+		this.driver = driver;
+	}
+	
+	public LandingPage_PageObject landingPage()
+	{
+		landing = new LandingPage_PageObject(driver);
+		
+		return landing;
+	}
+	
+	public OffersPage_PageObject offersPage()
+	{
+		offers = new OffersPage_PageObject(driver);
+		
+		return offers;
+		
+		//this is Supriya
+		//Mastee is the latest one
+		//Naa peru Viswanadh 9:48 , Nalagonda, Neenu machivadini, naa jolukostey bagundadu
+		//whats up
+	}
+
+}
